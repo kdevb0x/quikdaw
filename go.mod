a38214072b340c0d6e1faa8fda3b54a6f1741654
kdevcs <kris@codesiren.com>
2020-08-18 13:44:17 -0700

    add channel.go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module quikdaw
 
 go 1.15
+
+require github.com/therecipe/qt v0.0.0-20200817172549-0506a9f95d84
--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"os"
+
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/quick"
+	"github.com/therecipe/qt/widgets"
+)
+
+func init() {
+
+}
+
+func main() {
+	core.QCoreApplication_SetAttribute(core.Qt__AA_EnableHighDpiScaling, true)
+
+	app := widgets.NewQApplication(len(os.Args), os.Args)
+
+	view := quick.NewQQuickView(nil)
+
+	view.SetTitle("quikDAW")
+	view.SetResizeMode(quick.QQuickView__SizeRootObjectToView)
+	view.SetSource(core.NewQUrl3("qrc:/qml/main.qml", 0))
+	view.Show()
+
+	app.Exec()
+}
